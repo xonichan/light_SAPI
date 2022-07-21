192a4b6eb7ffd74fdba217b10fd3092afce60115
Nicholas <oni.chan.box@gmail.com>
2022-07-21 11:57:00 +0300

    Initial commit

---
+++ src/main/java/com/lsapi/light_SAPI/LightSapiApplication.java
@@ -0,0 +1,13 @@
+package com.lsapi.light_SAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightSapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LightSapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lsapi/light_SAPI/LightSapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lsapi.light_SAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LightSapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
